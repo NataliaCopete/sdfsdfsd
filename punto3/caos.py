64bc8b68516944e9c338dc98d68b0560be0f0edc
NataliaCopete <37335452+NataliaCopete@users.noreply.github.com>
2018-05-12 18:42:10 -0500

    Update caos.py

--- punto3/caos.py
+++ punto3/caos.py
@@ -2,12 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 datos=np.loadtxt("datos.txt")
-x=datos[0:10000,1]
-y=datos[0:10000,0]
-z=datos[0:10000,2]
+x=datos[:,1]
+y=datos[:,0]
+
 
 plt.plot(x,y)
-plt.savefig("grafica.png")
-plt.clf()
-plt.plot(y,z)
-plt.savefig("grafica1.png")
+plt.savefig("caos.pdf")
+
